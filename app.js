4ef6e711af19ce3c967adce03dc93b6232aff26f
Kazuhito Hokamura <k.hokamura@gmail.com>
2014-02-08 14:17:11 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+var http = require('http');
+var router = require('http-router');
+var routes = new router();
+var EventEmitter = require('events').EventEmitter;
+var emitter = new EventEmitter();
+var qs = require('querystring');
+
+routes
+  .post('/', function(req, res, next) {
+    var data = '';
+    req.on('data', function(chunk) {
+      data += chunk;
+    });
+    req.on('end', function() {
+      var type = req.headers['content-type'];
+      if (/application\/x-www-form-urlencoded/.test(type)) {
+        data = JSON.stringify(qs.parse(data));
+      }
+
+      emitter.emit('data', data);
+      res.writeHead(200, { 'Access-Control-Allow-Origin': '*' });
+      res.end('ok');
+    });
+  })
+  .get('/', function(req, res, next) {
+    if (req.headers['accept'] !== 'text/event-stream') return res.end('');
+
+    res.writeHead(200, {
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+      'Connection': 'keep-alive',
+      'Access-Control-Allow-Origin': '*',
+    });
+
+    emitter.on('data', function(data) {
+      res.write('data: ' + data + '\n\n');
+    });
+  });
+
+http.createServer(function(req, res) {
+  if (!routes.route(req, res)) {
+    res.writeHead(500);
+    res.end('Error');
+  }
+}).listen(process.env.PORT || 8080);
